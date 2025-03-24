bd2720b41c9f862e1f3b1e6e330ce795106d0722
LONGPC <ngvhoanglong@gmail.com>
2025-03-24 18:32:02 +0700

    add

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+tailwind.config = {
+    theme: {
+        screens: {
+            sm: "576px",
+            md: "768px",
+            lg: "992px",
+            xl: "1280px",
+        },
+        extend: {
+            colors: {
+                blueLight: `#6aa2e5`,
+                blueDark: "#485aa2",
+            },
+            fontFamily: {
+                outLine: ["Londrina Outline", "sans-serif"],
+            },
+        },
+    },
+};
